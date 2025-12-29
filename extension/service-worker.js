a1cbf8362a5bf3a7d3da1001d0544374d875d6b2
kumarstack55 <kumarstack55@gmail.com>
2025-12-29 23:23:32 +0900

    first commit

---
+++ extension/service-worker.js
@@ -0,0 +1,8 @@
+chrome.commands.onCommand.addListener((command) => {
+    console.log(`Command "${command}" triggered`);
+    switch (command) {
+      default:
+        console.log(`Command "${command}" is not supported.`);
+    }
+  }
+);
