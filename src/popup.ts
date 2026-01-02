13f4f98804aa1a086125de825996adddefa06247
kumarstack55 <kumarstack55@gmail.com>
2026-01-02 15:16:57 +0900

    fix

--- src/popup.ts
+++ src/popup.ts
@@ -1,63 +1,159 @@
-class UrlRewriter {
-  protected url: string;
+class UrlRewriterError extends Error {
+}
 
-  constructor(url: string) {
-    this.url = url;
+class UrlRewriter {
+  getName(): string {
+    throw new UrlRewriterError("Not implemented");
   }
 
-  rewrite(): string {
-    return this.url;
+  getRewritablePattern(): string {
+    throw new UrlRewriterError("Not implemented");
+  }
+
+  rewriteUrl(url: string): string {
+    throw new UrlRewriterError("Not implemented");
   }
 }
 
 class AmazonCoJpRewriter extends UrlRewriter {
-  rewrite(): string {
-    const u = new URL(this.url);
+  getName(): string {
+    return "Amazon.co.jp Rewriter";
+  }
+
+  getRewritablePattern(): string {
+    return "^https://www\\.amazon\\.co\\.jp/";
+  }
+
+  rewriteUrl(url: string): string {
+    const u = new URL(url);
 
     const pathname = u.pathname;
 
     const m = pathname.match(/\/dp\/\w+/);
-    if (m) {
-      const hostname = u.hostname;
-      const dpAsin = m[0];
-      return `https://${hostname}${dpAsin}/`;
+    if (!m) {
+      return url;
     }
 
+    const hostname = u.hostname;
+    const dpAsin = m[0];
+    return `https://${hostname}${dpAsin}/`;
+  }
+}
+
+class UrchinRemoverRewriter extends UrlRewriter {
+  getName(): string {
+    return "Urchin Remover Rewriter";
+  }
+
+  getRewritablePattern(): string {
+    return ".*[?&]utm_.*";
+  }
+
+  rewriteUrl(url: string): string {
+    const u = new URL(url);
+
+    // https://support.google.com/analytics/answer/11242870?hl=en
+    u.searchParams.delete('utm_source');
+    u.searchParams.delete('utm_medium');
+    u.searchParams.delete('utm_campaign');
+    u.searchParams.delete('utm_term');
+    u.searchParams.delete('utm_content');
+    u.searchParams.delete('utm_source_platform');
+    u.searchParams.delete('utm_creative_format');
+    u.searchParams.delete('utm_marketing_tactic');
+
+    return u.toString();
+  }
+}
+
+class Result {
+  private name: string;
+  private url: string;
+  constructor(name: string, url: string) {
+    this.name = name;
+    this.url = url;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getUrl(): string {
     return this.url;
   }
 }
 
+class ResultSet {
+  private results: Result[];
+
+  constructor(results: Result[]) {
+    this.results = results;
+  }
+
+  isEmpty(): boolean {
+    return this.results.length === 0;
+  }
+
+  getResults(): Result[] {
+    return this.results;
+  }
+}
+
+class RewriterMatcher {
+  private rewriters: Map<RegExp, UrlRewriter>;
+  constructor(rewriters: Map<RegExp, UrlRewriter>) {
+    this.rewriters = rewriters;
+  }
+
+  match(url: string): ResultSet {
+    const results: Result[] = [];
+
+    this.rewriters.forEach((rewriter, pattern) => {
+      if (pattern.test(url)) {
+        const rewrittenUrl = rewriter.rewriteUrl(url);
+        if (rewrittenUrl === url) {
+          return;
+        }
+        results.push(new Result(rewriter.getName(), rewrittenUrl));
+      }
+    });
+
+    return new ResultSet(results);
+  }
+}
+
 class RewriterFactory {
-  private rewriters: Map<RegExp, typeof UrlRewriter>;
+  private rewriters: Map<RegExp, UrlRewriter>;
 
   constructor() {
-    this.rewriters = new Map();
+    this.rewriters = new Map<RegExp, UrlRewriter>();
   }
 
-  addRewriter(urlPattern: string, rewriterClass: typeof UrlRewriter): void {
-    const re = new RegExp(urlPattern);
-    this.rewriters.set(re, rewriterClass);
+  addRewriter(rewriter: UrlRewriter): void {
+    const pattern = rewriter.getRewritablePattern();
+    const re = new RegExp(pattern);
+    this.rewriters.set(re, rewriter);
   }
 
-  getRewriter(url: string): UrlRewriter | null {
-    for (const [re, rewriterClass] of this.rewriters) {
-      if (re.test(url)) {
-        return new rewriterClass(url);
-      }
-    }
-    return null;
+  getRewriterMatcher(): RewriterMatcher {
+    return new RewriterMatcher(this.rewriters);
   }
 }
 
 function getRewritedUrl(url: string): string | null {
   const rewriterFactory = new RewriterFactory();
-  rewriterFactory.addRewriter(/^https:\/\/www\.amazon\.co\.jp\/.+/.source, AmazonCoJpRewriter);
+  rewriterFactory.addRewriter(new AmazonCoJpRewriter());
+  rewriterFactory.addRewriter(new UrchinRemoverRewriter());
 
-  const rewriter = rewriterFactory.getRewriter(url);
-  if (!rewriter) {
+  const rewriterMatcher = rewriterFactory.getRewriterMatcher();
+  const resultSet = rewriterMatcher.match(url);
+
+  if (resultSet.isEmpty()) {
     return null;
   }
-  return rewriter.rewrite();
+
+  // Return the first rewritten URL
+  return resultSet.getResults()[0].getUrl();
 }
 
 function updatePopupContent(url: string): void {
