ec70723c36fc8ef4043d871a9a04b0ffaca4b2f4
kumarstack55 <kumarstack55@gmail.com>
2026-01-02 14:49:00 +0900

    fix

--- extension/service-worker.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-chrome.commands.onCommand.addListener((command) => {
-    console.log(`Command "${command}" triggered`);
-    switch (command) {
-        default:
-            console.log(`Command "${command}" is not supported.`);
-    }
-});
